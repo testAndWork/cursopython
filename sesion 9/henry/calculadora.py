0a8641fbd9a0e2f1b055a52d78161157073a8bcc
testAndWork <142262518+testAndWork@users.noreply.github.com>
2023-08-21 16:14:38 -0600

    Add files via upload

---
+++ sesion 9/henry/calculadora.py
@@ -0,0 +1,14 @@
+def suma(x, y):
+    return x+y
+
+
+def resta(x, y):
+    return x-y
+
+
+def multiplicacion(x, y):
+    return x*y
+
+
+def divicion(x, y):
+    return x/y
---
+++ sesion 9/henry/test.py
@@ -0,0 +1,23 @@
+import calculadora as LC
+
+while True:
+
+    print('1- Suma \n2- Resta \n3- Miltiplicacion \n4- Divicion \n0- Para salir')
+    operacion = int(input("Ingrese el numero de la operacion: "))
+    n1 = float(input('Ingresa el 1 numero: '))
+    n2 = float(input('Ingresa el 1 numero: '))
+    if operacion == 1:
+        print(LC.suma(n1, n2))
+        print("")
+    elif operacion == 2:
+        print(LC.resta(n1, n2))
+        print("")
+    elif operacion == 3:
+        print(LC.multiplicacion(n1, n2))
+        print("")
+    elif operacion == 4:
+        print(LC.divicion(n1, n2))
+        print("")
+    elif operacion == 0:
+        print('Gracias por usar la calculadora')
+        break
